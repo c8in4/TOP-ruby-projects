8d9f94813cec613dc6fc94921b9fc602ae383c34
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-06-25 22:14:18 +1200

    Change to regex for identifying letters
    Only convert letters to preserve special characters in String.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,26 +1,32 @@
-ASCII_CODE_UPCASE_A = 65
-ASCII_CODE_UPCASE_Z = 90
+ASCII_CODE_A = 65
+ASCII_CODE_Z = 90
 
-ASCII_CODE_DOWNCASE_a = 97
-ASCII_CODE_DOWNCASE_z = 122
+ASCII_CODE_a = 97
+ASCII_CODE_z = 122
 
 NUMBER_OF_LETTERS_IN_ALPHABET = 26
 
 def caesar_cipher(str, shift)
 
-  arr_of_numbers = str.split("").map { |char| char.ord }    # TODO: only convert if char in range of a-z and A-Z ?
+  arr_of_ASCII = str.split("").map { |char|
+    char =~ /[a-zA-Z]/ ? char.ord : char
+  }
   
-  arr_of_shifted_numbers = arr_of_numbers.map { |num|
-    if num >= ASCII_CODE_UPCASE_A && num <= ASCII_CODE_UPCASE_Z
-      shift_chars(num, shift, ASCII_CODE_UPCASE_A, ASCII_CODE_UPCASE_Z)
-    elsif num >= ASCII_CODE_DOWNCASE_a && num <= ASCII_CODE_DOWNCASE_z
-      shift_chars(num, shift, ASCII_CODE_DOWNCASE_a, ASCII_CODE_DOWNCASE_z)
+  arr_of_shifted_numbers = arr_of_ASCII.map { |element|
+    unless element.is_a?(String)
+      if element >= ASCII_CODE_A && element <= ASCII_CODE_Z
+        shift_chars(element, shift, ASCII_CODE_A, ASCII_CODE_Z)
+      elsif element >= ASCII_CODE_a && element <= ASCII_CODE_z
+        shift_chars(element, shift, ASCII_CODE_a, ASCII_CODE_z)
+      end
     else
-      num
+      element
     end
   }
 
-  arr_of_shifted_numbers.map { |num| num.chr }.join("")     # TODO: only convert if num is of type int in range of 60 ?
+  arr_of_shifted_numbers.map { |element|
+    element.is_a?(String) ? element : element.chr
+  }.join("")
 end
 
 
@@ -37,16 +43,16 @@ def shift_chars(num, shift, range_bottom, range_top)
   shifted_char
 end
 
-puts "Enter a word or phrase:"
-str = gets.chomp
-puts "Enter a number:"
-offset = gets.chomp.to_i
+# puts "Enter a word or phrase:"
+# str = gets.chomp
+# puts "Enter a number:"
+# offset = gets.chomp.to_i
 
-p str
-p caesar_cipher(str, offset)
+# p str
+# p caesar_cipher(str, offset)
 
-p "AaBbCc XxYyZz"
-p caesar_cipher("AaBbCc XxYyZz", offset)
+# p "AaBbCc XxYyZz"
+# p caesar_cipher("AaBbCc XxYyZz", offset)
 
-# p caesar_cipher("What a string!", 5)
+p caesar_cipher("What a string!", 5)
 # => "Bmfy f xywnsl!"
